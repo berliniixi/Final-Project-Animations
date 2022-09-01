920a0f7592083d4b4fb38465761586eb27107fea
berliniixi <steliosevoix@gmail.com>
2022-09-01 15:03:59 +0300

    Initial Commit

---
+++ Assets/Y_Bot/Player Scripts/IKPickupPhone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKPickupPhone : MonoBehaviour
+{
+    
+    [SerializeField] private Transform target; // Phone
+    [SerializeField] private Transform playerHand;
+    [SerializeField] private float weight = 1;
+
+    private Animator _animator;
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    }
+}
---
+++ Assets/Y_Bot/Player Scripts/PlayerMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [Header("Player Components")] 
+    private Animator _animator;
+
+    [Header("Player Characteristics")] 
+    [SerializeField] private float playerSpeed = 10f;
+    [SerializeField] private float rotationSpeed = 100f;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float translation = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+
+       
+        
+        transform.Translate(0,0, translation);
+        transform.Rotate(0,rotation,0);
+        
+        if(translation > 0)
+        {
+            _animator.SetBool("isWalking", true);
+            _animator.SetFloat("direction" , 1f);
+        }
+        else if(translation < 0)
+        {
+            _animator.SetBool("isWalking", true);
+            _animator.SetFloat("direction" , -1f);
+        }
+        else
+        {
+            _animator.SetBool("isWalking" , false);
+            
+        }
+
+        Running();
+    }
+
+    void Running()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _animator.SetBool("isRunning" , true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            _animator.SetBool("isRunning" , false);
+        }
+    }
+    
+}
